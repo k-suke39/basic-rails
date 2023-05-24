2d326f3f35f909098ddaa53548ba9e95bbfd0415
kesuke390 <ksuke.sakuma@gmail.com>
2023-05-24 14:52:40 +0900

    ✨ add User validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
 
-  validates :first_name, presence: true, length: { maximum: 255 }
-  validates :last_name,  presence: true, length: { maximum: 255 }
+  validates :last_name,               presence: true, length: { maximum: 255 }
+  validates :first_name,              presence: true, length: { maximum: 255 }
+  validates :email,                   presence: true, uniqueness: true
+  validates :password,                presence: true, length: { minimum: 3 }
+  validates :password_confirmation,   presence: true, length: { minimum: 3 }
 end
