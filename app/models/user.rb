f0d6eff9f3d0b19474290cd364c02c70e5ea88d2
kesuke390 <ksuke.sakuma@gmail.com>
2023-05-24 18:35:39 +0900

    🐛 lint

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   validates :last_name,               presence: true, length: { maximum: 255 }
   validates :first_name,              presence: true, length: { maximum: 255 }
   validates :email,                   presence: true, uniqueness: true
-  validates :password,                presence: true, length: { minimum: 3 },if: -> { new_record? || changes[:crypted_password] }
+  validates :password,                presence: true, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password,                confirmation: true, if: -> { new_record? || changes[:crypted_password] }
-  validates :password_confirmation,   presence: true,if: -> { new_record? || changes[:crypted_password] }
+  validates :password_confirmation,   presence: true, if: -> { new_record? || changes[:crypted_password] }
 end
