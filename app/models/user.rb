c093e8c8e4d4a111492521fd403e9de49906976d
kesuke390 <ksuke.sakuma@gmail.com>
2023-05-24 16:54:54 +0900

    ✨ add signup form / for create function

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
-
   validates :last_name,               presence: true, length: { maximum: 255 }
   validates :first_name,              presence: true, length: { maximum: 255 }
   validates :email,                   presence: true, uniqueness: true
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root :to => "staticpages#top"
-
-  get 'users/new' => "users#new"
+  
   get 'login' => "user_sessions#new"
+  resources :users, only: %i[new create]
 
 end
