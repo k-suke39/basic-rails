d63641b88031390bb5d343aa9443a505441547d2
kesuke390 <ksuke.sakuma@gmail.com>
2023-05-24 18:35:27 +0900

    ✨ add create signup form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
   def create
     @user = User.new(user_params)
     p @user
@@ -9,11 +10,11 @@ class UsersController < ApplicationController
       redirect_to root_path
     else
       render :new
-
     end
   end
 
   private
+
   def user_params
     params.require(:user).permit(:last_name, :first_name, :email, :password, :password_confirmation)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root :to => "staticpages#top"
   
   get 'login' => "user_sessions#new"
+  post 'login' => "user_sessions#create"
+  delete 'logout' => "user_sessions#destroy"
   resources :users, only: %i[new create]
 
 end
