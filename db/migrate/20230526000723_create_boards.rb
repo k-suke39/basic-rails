f57f172724a9ffd14d9057fa696d8afd7d5b1c36
kesuke390 <ksuke.sakuma@gmail.com>
2023-05-26 10:48:16 +0900

    ✨ association

--- db/migrate/20230526000723_create_boards.rb
+++ db/migrate/20230526000723_create_boards.rb
@@ -3,7 +3,7 @@ class CreateBoards < ActiveRecord::Migration[7.0]
     create_table :boards do |t|
       t.string :title,    null: false
       t.text :body,       null: false
-      t.integer :user_id, null:false
+      t.integer :user_id, null: false
       t.timestamps
     end
   end
