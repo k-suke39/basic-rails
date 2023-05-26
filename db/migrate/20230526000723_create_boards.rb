dff9087e3194e72e9b7d4d6b9c3a69eff1328d92
kesuke390 <ksuke.sakuma@gmail.com>
2023-05-26 10:32:09 +0900

    ✨ add Board model & table

---
+++ db/migrate/20230526000723_create_boards.rb
@@ -0,0 +1,10 @@
+class CreateBoards < ActiveRecord::Migration[7.0]
+  def change
+    create_table :boards do |t|
+      t.string :title,    null: false
+      t.text :body,       null: false
+      t.integer :user_id, null:false
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_23_074841) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_26_000723) do
+  create_table "boards", charset: "utf8mb4", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "body", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "first_name", null: false
     t.string "last_name", null: false
