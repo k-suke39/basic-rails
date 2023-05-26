72a8262686bd6c175ac1ee0e53617bc65aa09376
kesuke390 <ksuke.sakuma@gmail.com>
2023-05-26 17:13:31 +0900

    ✨ add board form

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,6 +1,27 @@
 class BoardsController < ApplicationController
-  before_action :login?, only: %i[index]
+  before_action :login?
   def index
     @boards = Board.all.includes(:user).order(created_at: :desc)
+  end
+
+  def new
+    @board = Board.new
+  end
+
+  def create
+    @board = Board.new(board_params)
+    @board.user_id = current_user.id
+    if @board.save
+      redirect_to boards_path, success: t('boards.create.successful')
+    else
+      flash.now[:danger] = t('boards.create.failure')
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def board_params
+    params.require(:board).permit(:title, :body)
   end
 end
