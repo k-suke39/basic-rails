332a1480a55a0b01caf0310097d87854de17248b
kesuke390 <ksuke.sakuma@gmail.com>
2023-05-29 19:15:38 +0900

    ✨ add turbo-frame

--- Gemfile
+++ Gemfile
@@ -47,6 +47,7 @@ gem 'rails-i18n', '~> 7.0.0'
 gem 'draper', '4.0.2'
 gem 'faker'
 gem 'carrierwave', '2.2.2'
+gem 'turbo-rails', "1.1.1"
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -2,12 +2,12 @@ class BookmarksController < ApplicationController
   def create
     @board = Board.find(params[:board_id])
     current_user.bookmark(@board)
-    redirect_to boards_path, success: t('bookmarks.create.successful')
+    redirect_to boards_path
   end
 
   def destroy
     @board = current_user.bookmarks.find(params[:board_id]).board
     @board.delete
-    redirect_to boards_path, danger: t('bookmarks.destroy.successful'), status: :see_other
+    redirect_to boards_path status: :see_other
   end
 end
